5f33ace6d8bb1e9bbf0e08ecb56e0b05e722a688
quynhanh170504 <huynhanh.170504@gmail.com>
2025-10-23 21:17:39 +0700

    add: webhook to update order status after paid

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 import { HttpModule } from '@nestjs/axios';
+import { OrderModule } from '../order/order.module';
 
 @Module({
   controllers: [PaymentController],
   providers: [PaymentService],
-  imports: [HttpModule]
+  imports: [HttpModule, OrderModule]
 })
 export class PaymentModule { }
