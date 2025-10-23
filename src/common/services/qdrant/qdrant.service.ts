bc90b9253f9de45fe135bf5d8a4ed727e28722c2
DatHuynhCoder <htdat2004@gmail.com>
2025-10-23 21:13:10 +0700

    feat: qdrant service: connect to qdrant DB, embedding and search

--- src/common/services/qdrant/qdrant.service.ts
+++ src/common/services/qdrant/qdrant.service.ts
@@ -2,6 +2,8 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { QdrantClient } from "@qdrant/qdrant-js";
+import { FlowerDocument } from "../../../modules/flower/schemas/flower.schema"
+import { trimHTMLTags } from "src/utils/trimHTMLTags";
 
 @Injectable()
 export class QdrantService implements OnModuleInit {
@@ -54,5 +56,67 @@ export class QdrantService implements OnModuleInit {
         }
       )
     }
+  }
+
+  //Embidding the text and return the vector
+  private async getEmbedding(text: string) {
+    const embeddingModel = this.genAI.getGenerativeModel({ model: 'gemini-embedding-001' })
+    const result = await embeddingModel.embedContent(text);
+    return result.embedding.values;
+  }
+
+  //Vectorize FlowerInfo and store it on Qdrant DB
+  async embedAndStoreFlower(flower: FlowerDocument) {
+    const flowerTypes = flower.types.join(' ');
+    const textToEmbed =
+      `Tên hoa: ${flower.name}. 
+       Mô tả: ${trimHTMLTags(flower.description)}. 
+       Giá tiền: ${flower.price} . 
+       Đánh giá: ${flower.rating}.
+       Dịp lễ: ${flower.occasion}.
+       Các loại hoa: ${flowerTypes}.
+       Kiểu dáng hoa: ${flower.form}.
+       Số lượng bán được: ${flower.quantitySold}
+      `;
+    
+    //get vector by embedding flower info
+    const vector = await this.getEmbedding(textToEmbed);
+
+    //Insert vector into Qdrant DB
+    await this.client.upsert(this.collectionName, {
+      wait: true,
+      points: [
+        {
+          id: flower._id.toString(),
+          vector: vector,
+          //Payload fields are used to filter
+          payload: {
+            price: flower.price,
+            rating: flower.rating,
+            stockQuantity: flower.stockQuantity,
+            quantitySold: flower.quantitySold,
+            numRating: flower.numRating
+          }
+        }
+      ]
+    })
+    console.log(`Vector hóa và lưu hoa: ${flower.name} thành công`)
+  }
+  
+  //Find flowers using vector similarity
+  async searchSimilarFlowers(query: string, limit = 3) {
+    //get vector by embedding query text
+    const queryVector = await this.getEmbedding(query);
+
+    const searchResult = await this.client.search(this.collectionName, {
+      vector: queryVector,
+      limit: limit,
+      with_payload: false
+    })
+
+    //get only ids, then we get full flowers info by query them on mongoDB
+    const result = searchResult.map((result) => result.id.toString());
+
+    return result;
   }
 }
